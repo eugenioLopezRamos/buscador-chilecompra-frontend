9072d955e6a649e3d4ee5c1bca7596c9995ae91c
Eugenio <eugenionlopez@gmail.com>
2017-02-22 13:22:05 -0300

    Modifies package json to make enzyme work...

--- src/components/test/inputs/AutoFillerInput.spec.jsx
+++ src/components/test/inputs/AutoFillerInput.spec.jsx
@@ -1,4 +1,56 @@
-import React from 'react';
+import React from 'react'
+import { shallow } from 'enzyme'
+import AutoFillerInput from '../../inputs/AutoFillerInput.jsx';
+
+
+
+function setup() {
+  const props = {
+    addTodo: jest.fn()
+  }
+
+  const enzymeWrapper = shallow(<AutoFillerInput {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+
+describe('components', () => {
+  describe('AutoFillerInput', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setup()
+
+      expect(enzymeWrapper.find('div').hasClass('selection-container')).toBe(true);
+
+      expect(enzymeWrapper.find('input').hasClass("col-xs-12 col-md-10 col-lg-4 no-gutter" )).toBe(true);
+      expect(enzymeWrapper.find('select')).toBe(true);
+
+      const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
+      expect(todoInputProps.newTodo).toBe(true)
+      expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+    })
+
+    it('should call addTodo if length of text is greater than 0', () => {
+      const { enzymeWrapper, props } = setup()
+      const input = enzymeWrapper.find('TodoTextInput')
+      input.props().onSave('')
+      expect(props.addTodo.mock.calls.length).toBe(0)
+      input.props().onSave('Use Redux')
+      expect(props.addTodo.mock.calls.length).toBe(1)
+    })
+  })
+})
+
+
+
+
+
+
+
+/*import React from 'react';
 
 const AutoFillerInput = (props) => {
 
@@ -39,5 +91,5 @@ const AutoFillerInput = (props) => {
         )
 }
 export default AutoFillerInput;
-
+*/
 
