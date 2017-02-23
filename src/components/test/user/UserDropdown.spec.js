99f1cf8cf332f68c69bf64bf5bff5daa0d7383a8
Eugenio <eugenionlopez@gmail.com>
2017-02-23 16:36:25 -0300

    Adds UserDropdown tests

--- src/components/test/user/UserDropdown.spec.js
+++ src/components/test/user/UserDropdown.spec.js
@@ -1,49 +1,71 @@
 import React from 'react';
-import {Link} from 'react-router';
-import Notifications from '../../user/Notifications';
+import {shallow} from 'enzyme';
+import UserDropdown from '../../user/UserDropdown';
 
-const UserDropdown = (props) => {
 
-    const handleLogout = () => {
-        props.handleLogout();
+function setup() {
+    const props = {
+        handleLogout: jest.fn(),
+        toggleNotifications: jest.fn(),
+        deleteNotification: jest.fn(),
+        handleNotificationsClick: jest.fn(),
+        notifications: {
+            "9": "Cambios en licitacion XXX-YYY-ZZZZ",
+            "15": "Cambios en la licitacion ZZZ-YYY-XXXX"
+        },
+        showNotifications: true,
+        visible: true
     }
-    const handleNotificationsClick = (event) => {
-        event.stopPropagation();
-        props.toggleNotifications();
+
+    const wrapper = shallow(<UserDropdown {...props}/>);
+
+    return {
+        props,
+        wrapper
     }
-    
-    const notificationStyle = (() => {
-        if(props.showNotifications) {
-            return "dropdown-menu-list-item active";
-        }
-        return "dropdown-menu-list-item";
-    })();
 
-
-    return (
-        <div className="dropdown">
-
-            <ul className={props.visible ? "dropdown-menu dropdown-menu-right dropdown-visible" : "dropdown-menu dropdown-menu-right"} aria-labelledby="dropdownMenu1">
-                <li><Link to="/inicio">Página Principal</Link></li>
-                <li className={notificationStyle} onClick={handleNotificationsClick}>
-                    Notificaciones
-                    <Notifications 
-                           notifications={props.notifications}
-                           show={props.showNotifications}
-                           toggleNotifications={props.toggleNotifications}
-                           deleteNotification={props.deleteNotification}
-                    />
-                </li>
-
-                <li><Link to="/perfil">Modificar datos</Link></li>
-                <li><Link to="/busqueda">Buscar</Link></li>
-                <li role="separator" className="divider"></li>
-                <li onClick={handleLogout}><Link to="/">Salir de la aplicación</Link></li>
-            </ul>        
-        </div>
-
-    )
 
 }
 
-export default UserDropdown;
+describe('Component', () => {
+
+    describe('UserDropdown', () => {
+        const {wrapper, props} = setup();
+
+        it('Should render self and subcomponents', () => {
+
+            expect(wrapper.find('div.dropdown').length).toEqual(1);
+            expect(wrapper.find('ul.dropdown-menu.dropdown-menu-right.dropdown-visible').length).toBe(1);
+            expect(wrapper.find('li').length).toEqual(6);
+            expect(wrapper.find('Link').length).toEqual(4);
+            expect(wrapper.find('Notifications').length).toEqual(1);
+
+            const notificationsComponent = wrapper.find('Notifications');
+            expect(notificationsComponent.props().notifications).toEqual(props.notifications);
+            expect(notificationsComponent.props().show).toEqual(props.showNotifications);
+            expect(notificationsComponent.props().toggleNotifications).toEqual(props.toggleNotifications);
+            expect(notificationsComponent.props().deleteNotification).toEqual(props.deleteNotification);
+
+
+
+        });
+
+        it('Should invoke functions from props correctly', () => {
+        
+            const logoutLi = wrapper.find('li').at(wrapper.find('li').length - 1);
+            expect(props.handleLogout.mock.calls.length).toEqual(0);
+            logoutLi.simulate("click");
+            expect(props.handleLogout.mock.calls.length).toEqual(1);
+
+            const notificationsComponent = wrapper.find('Notifications');
+            expect(props.toggleNotifications.mock.calls.length).toEqual(0);
+            notificationsComponent.props().toggleNotifications()
+            expect(props.toggleNotifications.mock.calls.length).toEqual(1);
+
+            expect(props.deleteNotification.mock.calls.length).toEqual(0);
+            notificationsComponent.props().deleteNotification(<div></div>);
+            expect(props.deleteNotification.mock.calls.length).toEqual(1);
+        });
+
+    });
+});
