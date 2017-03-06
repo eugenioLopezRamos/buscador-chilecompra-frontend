3c7bd75257b92f83749eaaea9fb90bd76f3f1827
Eugenio <eugenionlopez@gmail.com>
2017-03-06 16:30:18 -0300

    adds User searches reducer tests

--- src/reducers/test/userSearchesReducer.spec.js
+++ src/reducers/test/userSearchesReducer.spec.js
@@ -1,42 +1,62 @@
 import * as types from '../../constants/actionTypes';
 import initialState from '../initialState';
-
 import objectAssign from 'object-assign';
+import {userSearchesReducer as reducer} from '../userSearchesReducer';
 
 
-export const userSearchesReducer = (state = initialState.userSearches, action) => {
-   // let newState = objectAssign({}, state);
+describe('reducer', () => {
+    describe('user searches reducer', () => {
+        it('Should return the correct states', () => {
+            
+            let action = {type: undefined, value: undefined};
+            let initialValue = initialState.userSearches;
+            let expectedValue;
+            let mockValue;
 
-    switch(action.type) {
+            expect(initialValue).toEqual(reducer(undefined, action));
 
-        case types.USER_GET_SEARCHES_SUCCESS:
-            return objectAssign({}, state, action.value.searches);
+            mockValue = {
+                         name: ["aaa", "bbb", "ccc"],
+                         value: [{key1: "aaa", key2: "bbb"}, {key1: "zzz", key2: "yyy"}, {key1: "mmm", key2: "nnn"}],
+                         id: [1,2,3]
+            }
+            action = {type: types.USER_GET_SEARCHES_SUCCESS, value: {searches: mockValue}};
+            expectedValue = action.value.searches;
 
-        case types.USER_GET_SEARCHES_FAILURE:
-            return objectAssign({}, state, initialState.userSearches);
+            expect(expectedValue).toEqual(reducer(undefined, action));
 
-        case types.USER_CREATE_SEARCHES_SUCCESS:
-            return objectAssign({}, state, action.value.searches);
+            action = {type: types.USER_GET_SEARCHES_FAILURE};
+            expectedValue = initialValue;
+            expect(expectedValue).toEqual(reducer(undefined, action));
 
-        case types.USER_CREATE_SEARCHES_FAILURE:
-            return objectAssign({}, state, initialState.userSearches);
+            mockValue.name.push("ddd");
+            mockValue.value.push({key1: "123", key2: "321"});
+            mockValue.id.push(4);
 
-        case types.USER_UPDATE_SEARCHES_SUCCESS:
+            action = {type: types.USER_CREATE_SEARCHES_SUCCESS, value: {searches: mockValue}};
+            expectedValue = action.value.searches;
+            expect(expectedValue).toEqual(reducer(undefined, action));
 
-            return objectAssign({}, state, action.value.searches);
+            action = {type: types.USER_CREATE_SEARCHES_FAILURE};
+            expectedValue = initialValue;
+            expect(expectedValue).toEqual(reducer(undefined, action));
 
-        case types.USER_UPDATE_SEARCHES_FAILURE:
-            return objectAssign({}, state);
+            mockValue.name = mockValue.name.slice(1);
+            mockValue.value = mockValue.value.slice(1);
+            mockValue.id = mockValue.id.slice(1);
 
-        case types.USER_DELETE_SEARCHES_SUCCESS:
-            return objectAssign({}, state, action.value.searches);
+            action = {type: types.USER_DELETE_SEARCHES_SUCCESS, value: {searches: mockValue}};
+            expectedValue = action.value.searches;
+            expect(expectedValue).toEqual(reducer(undefined, action));
 
-        case types.USER_DELETE_SEARCHES_FAILURE:
-            return objectAssign({}, state);
-        case types.USER_LOGOUT_SUCCESS:
-            return initialState.userSearches;
 
-        default:
-            return state;
-    }
-}
+            action = {type: types.USER_DELETE_SEARCHES_FAILURE};
+            expectedValue = initialValue;
+            expect(expectedValue).toEqual(reducer(undefined, action));
+
+            action = {type: types.USER_LOGOUT_SUCCESS};
+            expectedValue = initialValue;
+            expect(expectedValue).toEqual(reducer(undefined, action));
+        });
+    });
+});
