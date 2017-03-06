f5fa8de865af551f8d2deef9ccd80bb00a0b4af5
Eugenio <eugenionlopez@gmail.com>
2017-03-06 15:24:20 -0300

    Adds search query values reducer tests

--- src/reducers/test/searchQueryValuesReducer.spec.js
+++ src/reducers/test/searchQueryValuesReducer.spec.js
@@ -2,24 +2,60 @@ import * as types from '../../constants/actionTypes';
 import initialState from '../initialState';
 import objectAssign from 'object-assign';
 import moment from 'moment';
-
-export default function searchQueryValuesReducer(state = initialState.searchQueryValues, action) {
-
-    switch(action.type) {
-        
-        case types.FETCH_CHILECOMPRA_DATA_SUCCESS:
-            return objectAssign({}, state, action.query);
-        case types.FETCH_CHILECOMPRA_DATA_FAILURE:
-            return objectAssign({}, state, action.query);
-
-        case types.ONLOAD_FETCH_ORG_PUB_SUCCESS:
-            return objectAssign({}, state, {organismosPublicosFilter: ""}, {organismosPublicosFilteredSubset: action.value});
+import searchQueryValuesReducer from '../searchQueryValuesReducer';
 
 
-        default:
-            return state;
+describe('Reducer', () => {
+    describe('searchQueryValuesReducer', () => {
+        it('Should return the correct states', () => {
 
-    }
+            const reducer = searchQueryValuesReducer;
 
-}
+            let action = {type: undefined, query: undefined};
+            let initialValue = initialState.searchQueryValues;
 
+            // same as initialValue at definition, will change later;
+            let queryValues = {
+                organismosPublicosFilter: "",
+                selectedOrganismoPublico: "*",
+                organismosPublicosFilteredSubset: [{"*":"Todos"}],
+                codigoLicitacion: "",
+                startDate: "1234",
+                alwaysFromToday: false,
+                endDate: "1234",
+                alwaysToToday: false,
+                palabrasClave: "",
+                selectedEstadoLicitacion: "",
+                rutProveedor: "",
+                offset: 0,
+                order_by: {fields: ["mockField"], order: "descending"}
+            }
+            // should return initialState;
+            expect(initialValue).toEqual(reducer(undefined, action));
+
+
+            //FETCH CHILECOMPRA DATA
+                //SUCCESS
+            action = {type: types.FETCH_CHILECOMPRA_DATA_SUCCESS, query: queryValues};
+            queryValues.organismosPublicosFilter = "my very own filter";
+
+            let expectedValue = objectAssign({}, initialValue, queryValues);
+            expect(expectedValue).toEqual(reducer(undefined, action));
+
+                //FAILURE
+            action = {type: types.FETCH_CHILECOMPRA_DATA_FAILURE, query: queryValues};
+            expect(expectedValue).toEqual(reducer(undefined, action));
+
+            let filteredOrganismos = {1: "Org 1", 2: "Org 2"};
+
+            //FETCH ORGANISMOS PUBLICOS
+            action = {type: types.ONLOAD_FETCH_ORG_PUB_SUCCESS, value: filteredOrganismos};
+            expectedValue = objectAssign({}, initialValue, {organismosPublicosFilter: ""}, {organismosPublicosFilteredSubset: filteredOrganismos});
+            expect(expectedValue).toEqual(reducer(undefined, action));
+
+
+
+
+        });
+    });
+})
